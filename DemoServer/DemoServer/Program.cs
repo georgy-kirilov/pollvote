25c9546d04135a4d9a3bd6caf4a7aebedcf4ab61
= <georgynk17@gmail.com>
2023-04-30 15:54:17 +0300

    Create /html route which returns a simple HTML page with dynamically rendered content

--- DemoServer/DemoServer/Program.cs
+++ DemoServer/DemoServer/Program.cs
@@ -9,4 +9,10 @@ app.MapGet("/", (int a, string b) =>
 
 app.MapGet("/demo", () => "Hello Edo!");
 
+app.MapGet("/html", (string title) =>
+{
+    // Make SQL query request to the DB
+    return Results.Content($"<h1 style=\"color: green\">{title}</h1>", "text/html");
+});
+
 app.Run();
